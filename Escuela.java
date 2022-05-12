480c7bc1fc3ae6f27d74730ef05cf7b3e200175d
OctavioCortez24 <84475343+OctavioCortez24@users.noreply.github.com>
2022-05-12 15:05:35 -0300

    Add files via upload

---
+++ Escuela.java
@@ -0,0 +1,33 @@
+package Clase2Semana1;
+
+public class Escuela {
+
+    private String nombre;
+    private String nivel;
+
+    public Escuela() {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre
+                + "\nNivel: " + nivel;
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,11 @@
+package Clase2Semana1;
+
+public class Principal {
+    public static void main(String[] args) {
+        Grado grado1=new Grado();
+       Escuela esc1=(Escuela)grado1;
+        
+        System.out.println(esc1);
+
+    }
+}
