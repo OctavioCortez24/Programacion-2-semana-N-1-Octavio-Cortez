e19255f3536e0f75ebffd12e2f91d3628bf46895
OctavioCortez24 <oc31549@gmail.com>
2022-06-10 13:03:04 -0300

    Mejore la clase

--- Clase N°2/Contac Book/view.java
+++ Clase N°2/Contac Book/view.java
@@ -27,4 +27,30 @@ public class view {
         return c;
 
     }
+    public static Contact deleteContact(ContactBook cB) {
+        boolean topListDelCont;
+        System.out.println(cB);
+        Scanner LeerNumer=new Scanner(System.in);
+        Scanner Leer=new Scanner(System.in);
+            System.out.println("Select the contact you want to delete:");
+            System.out.print("Enter the name of contact:");
+            String nameCont=Leer.nextLine();
+            System.out.print("Enter the email of contact:");
+            String emailCont=Leer.nextLine();
+            System.out.print("Enter the phone number of contact:");
+            String numerCont=Leer.nextLine();
+            System.out.print("Enter the topList of contact:");
+            System.out.println("[1]-->True");
+            System.out.println("[2]-->False");
+            int decicion=LeerNumer.nextInt();
+            if (decicion==1){
+                topListDelCont=true;
+            }else {
+                topListDelCont=false;
+            }
+           Contact c=new Contact(nameCont,emailCont,numerCont,topListDelCont);
+
+        return c;
+
+    }
 }
