6ae6ad7ccbaa35b617a193918cf59d6ba985e400
OctavioCortez24 <oc31549@gmail.com>
2022-06-10 16:14:13 -0300

    Casi lo termino

--- Clase N°2/Contac Book/Controller.java
+++ Clase N°2/Contac Book/Controller.java
@@ -6,39 +6,12 @@ import java.util.*;
 public class Controller {
 
     public static void main(String[] args) {
-        ArrayList<ContactBook>memoria = new ArrayList<>();
+        ArrayList<ContactBook>contactBooks = new ArrayList<>();
+        view.menu(contactBooks);
 
 
-
-
-        Scanner LeerNumer=new Scanner(System.in);
-        int decicion=0;
-
-        do{
-            System.out.println("Program to create contactBook");
-            System.out.println("[1]-->Create a new ContactBook");
-            System.out.println("[2]-->Add contact to contactBook");
-            System.out.println("[9]-->Exit");
-            decicion=LeerNumer.nextInt();
-            if (decicion==1){
-               ContactBook contactBookObj=view.createAContactBook();
-
-                memoria.add(contactBookObj);
-            } else if (decicion==2) {
-                if (memoria==null||memoria.size()==0){
-                    System.out.println("There is no contactBook");
-                }else{
-                    System.out.println("Choose the contact book");
-                    for (int i=0;i<memoria.size();i++){
-                        System.out.println(i+" "+memoria.get(i));
-                    }
-                    int seleccion=LeerNumer.nextInt();
-                    ContactBook contactBookSelecion=memoria.get(seleccion);
-                    view.menu2(contactBookSelecion);
-                }
-            }
-
-        }while (decicion!=9);
-
+       for (int i=0;i<contactBooks.size();i++){
+           System.out.println(contactBooks.get(i));
+       }
     }
 }
--- Clase N°2/Contac Book/view.java
+++ Clase N°2/Contac Book/view.java
@@ -1,11 +1,39 @@
 package ClaseN2SemanaN3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class view {
 
-    public static void menu(){
+    public static void menu(ArrayList<ContactBook> memoria){
+        Scanner LeerNumer=new Scanner(System.in);
+        int decicion=0;
 
+        do{
+            System.out.println("Program to create contactBook");
+            System.out.println("[1]-->Create a new ContactBook");
+            System.out.println("[2]-->Add contact to contactBook");
+            System.out.println("[9]-->Exit");
+            decicion=LeerNumer.nextInt();
+            if (decicion==1){
+                ContactBook contactBookObj=view.createAContactBook();
+
+                memoria.add(contactBookObj);
+            } else if (decicion==2) {
+                if (memoria==null||memoria.size()==0){
+                    System.out.println("There is no contactBook");
+                }else{
+                    System.out.println("Choose the contact book");
+                    for (int i=0;i<memoria.size();i++){
+                        System.out.println(i+" "+memoria.get(i));
+                    }
+                    int seleccion=LeerNumer.nextInt();
+                    ContactBook contactBookSelecion=memoria.get(seleccion);
+                    view.menu2(contactBookSelecion);
+                }
+            }
+
+        }while (decicion!=9);
     }
     public static ContactBook createAContactBook(){
         Scanner Leer=new Scanner(System.in);
