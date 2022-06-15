3a800367c3fefb459cc1a2d1c6bc094c26376ca3
OctavioCortez24 <oc31549@gmail.com>
2022-06-15 19:09:42 -0300

    Subo programa

--- Clase N°2/Contac Book/view.java
+++ Clase N°2/Contac Book/view.java
@@ -1,85 +1,98 @@
 package ClaseN2SemanaN3;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class view {
 
-    public static void menu(ArrayList<ContactBook> memoria){
-        Scanner LeerNumer=new Scanner(System.in);
-        int decicion=0;
+    public static void menu(ArrayList<ContactBook> memoria) {
+        Scanner LeerNumer = new Scanner(System.in);
+        int decicion = 0;
+        try {
+            do {
+                System.out.println("Program to create contactBook");
+                System.out.println("[1]-->Create a new ContactBook");
+                System.out.println("[2]-->Add contact to contactBook");
+                System.out.println("[9]-->Exit");
+                decicion = LeerNumer.nextInt();
+                if (decicion == 1) {
+                    ContactBook contactBookObj = view.createAContactBook();
 
-        do{
-            System.out.println("Program to create contactBook");
-            System.out.println("[1]-->Create a new ContactBook");
-            System.out.println("[2]-->Add contact to contactBook");
-            System.out.println("[9]-->Exit");
-            decicion=LeerNumer.nextInt();
-            if (decicion==1){
-                ContactBook contactBookObj=view.createAContactBook();
-
-                memoria.add(contactBookObj);
-            } else if (decicion==2) {
-                if (memoria==null||memoria.size()==0){
-                    System.out.println("There is no contactBook");
-                }else{
-                    System.out.println("Choose the contact book");
-                    for (int i=0;i<memoria.size();i++){
-                        System.out.println("["+i+"]--> "+memoria.get(i));
+                    memoria.add(contactBookObj);
+                } else if (decicion == 2) {
+                    if (memoria == null || memoria.size() == 0) {
+                        System.out.println("There is no contactBook");
+                    } else {
+                        System.out.println("Choose the contact book");
+                        for (int i = 0; i < memoria.size(); i++) {
+                            System.out.println("[" + i + "]--> " + memoria.get(i));
+                        }
+                        int seleccion = LeerNumer.nextInt();
+                        ContactBook contactBookSelecion = memoria.get(seleccion);
+                        view.menu2(contactBookSelecion);
                     }
-                    int seleccion=LeerNumer.nextInt();
-                    ContactBook contactBookSelecion=memoria.get(seleccion);
-                    view.menu2(contactBookSelecion);
                 }
-            }
 
-        }while (decicion!=9);
+            } while (decicion != 9);
+        } catch (InputMismatchException e) {
+            System.out.println("Enter a number");
+        }
+
     }
-    public static ContactBook createAContactBook(){
-        Scanner Leer=new Scanner(System.in);
+
+    public static ContactBook createAContactBook() {
+        Scanner Leer = new Scanner(System.in);
         System.out.println("Enter the name of contactBook");
-        String nameContactBook=Leer.nextLine();
-        ContactBook libreta=new ContactBook(nameContactBook);
+        String nameContactBook = Leer.nextLine();
+        ContactBook libreta = new ContactBook(nameContactBook);
         return libreta;
     }
-    public static void menu2(ContactBook contactBookObj){
+
+    public static void menu2(ContactBook contactBookObj) {
         Scanner LeerNumer = new Scanner(System.in);
 
+        try {
+            int decision = 0;
+            System.out.println("Program to create a list of contact");
+            do {
+                System.out.println("[0]-->Exit");
+                System.out.println("[1]-->Create a new contact");
+                System.out.println("[2]-->Delete contact");
+                System.out.println("[3]-->Show contact book");
 
-        int decision = 0;
-        System.out.println("Program to create a list of contact");
-        do {
-            System.out.println("[0]-->Exit");
-            System.out.println("[1]-->Create a new contact");
-            System.out.println("[2]-->Delete contact");
-            System.out.println("[3]-->Show contact book");
+                decision = LeerNumer.nextInt();
+                if (decision == 1) {
+                    Contact contact = view.createContact();
+                    contactBookObj.addContact(contact);
 
-            decision = LeerNumer.nextInt();
-            if (decision == 1) {
-                Contact contact=view.createContact();
-                contactBookObj.addContact(contact);
+                } else if (decision == 2) {
+                    System.out.println("Do you want to delete a contact?");
+                    System.out.println("[1]--> Yes");
+                    System.out.println("[2]--> No");
+                    int delOrNotDel = LeerNumer.nextInt();
+                    if (delOrNotDel == 1) {
+                        Contact c = view.deleteContact(contactBookObj);
+                        contactBookObj.delContact(c);
+                    }
 
-            } else if (decision == 2) {
-                System.out.println("Do you want to delete a contact?");
-                System.out.println("[1]--> Yes");
-                System.out.println("[2]--> No");
-                int delOrNotDel= LeerNumer.nextInt();
-                if (delOrNotDel==1) {
-                    Contact c=view.deleteContact(contactBookObj);
-                    contactBookObj.delContact(c);
+
+                } else if (decision == 3) {
+                    ArrayList<Contact> arrayParaMostrar = contactBookObj.mostrar();
+                    if (arrayParaMostrar == null || arrayParaMostrar.size() == 0) {
+                        System.out.println("There are no contacts");
+                    } else {
+                        System.out.println(contactBookObj.toString('|'));
+                    }
                 }
-
-
-            }else if(decision==3){
-                ArrayList<Contact> arrayParaMostrar=contactBookObj.mostrar();
-               if (arrayParaMostrar==null||arrayParaMostrar.size()==0){
-                   System.out.println("There are no contacts");
-               }else{
-                   System.out.println(contactBookObj.toString('|'));
-               }
-            }
-        } while (decision != 0);
+            } while (decision != 0);
+        } catch (InputMismatchException e) {
+            System.out.println("Enter a number");
+        }
     }
+
+
+
 
     public static Contact createContact(){
         boolean topList;
